896372e46590632d1d067965fa82643647ebdd8a
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-23 08:35:02 +1000

    Added new files

--- store.mortalkombat.parfunc.php
+++ store.mortalkombat.parfunc.php
@@ -1,41 +1,23 @@
 <?php
 
 $proUseWeapon = shopFor('.', 'weapon');
-$subUseWeapon = shopFor('./'.$sub, 'weapon');
-$objUseWeapon = shopFor('./'.$obj, 'weapon');
+$subUseWeapon = shopFor($sub, 'weapon');
+$objUseWeapon = shopFor($obj, 'weapon');
 
 if ($proUseWeapon !== null) {
-    $proWeaponObjID = $proUseWeapon['object_id'];
-    $proWeaponObjClass = $proUseWeapon['object_class'];
-    $proWeaponName = $proUseWeapon['name'];
-    $proWeaponPrice = $proUseWeapon['price'] * ratioCalc($spacedictus[$subLingo]['curval'], $spacedictus[$objLingo]['curval']);
     $proForce = $proUseWeapon['damage'];
 } else {
-    $proWeaponName = $spacedictus[$lingua]['melee'];
-    $proWeaponPrice = 0;
     $proForce = 1;
 }
 
 if ($subUseWeapon !== null) {
-    $subWeaponObjID = $subUseWeapon['object_id'];
-    $subWeaponObjClass = $subUseWeapon['object_class'];
-    $subWeaponName = $subUseWeapon['name'];
-    $subWeaponPrice = $subUseWeapon['price'] * ratioCalc($spacedictus[$subLingo]['curval'], $spacedictus[$objLingo]['curval']);
     $subForce = $subUseWeapon['damage'];
 } else {
-    $subWeaponName = $spacedictus[$lingua]['melee'];
-    $subWeaponPrice = 0;
     $subForce = 1;
 }
 
 if ($objUseWeapon !== null) {
-    $objWeaponObjID = $objUseWeapon['object_id'];
-    $objWeaponObjClass = $objUseWeapon['object_class'];
-    $objWeaponName = $objUseWeapon['name'];
-    $objWeaponPrice = $objUseWeapon['price'] * ratioCalc($spacedictus[$subLingo]['curval'], $spacedictus[$objLingo]['curval']);
     $objForce = $objUseWeapon['damage'];
 } else {
-    $objWeaponName = $spacedictus[$lingua]['melee'];
-    $objWeaponPrice = 0;
     $objForce = 1;
 }
