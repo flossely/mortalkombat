ef2c1ca913d1267c162621b6c2c8db91c37e147e
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-03 12:46:49 +0000

    Latest update

--- onrr.mortalkombat.parfunc.php
+++ onrr.mortalkombat.parfunc.php
@@ -6,17 +6,14 @@ $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "punch") {
     $objRating -= $subPunchForce;
-    $subRating += $subPunchForce;
     $subScore += $subPunchForce;
     echo $turnNum.' : '.$subFullName.' '.$spacedictus[$proLingo]["punch"].' ('.$subPunchForce.') '.$objFullName.'<br>';
 } elseif ($subAction == "kick") {
     $objRating -= $subKickForce;
-    $subRating += $subKickForce;
     $subScore += $subKickForce;
     echo $turnNum.' : '.$subFullName.' '.$spacedictus[$proLingo]["kick"].' ('.$subKickForce.') '.$objFullName.'<br>';
 } elseif ($subAction == "grapple") {
     $objRating -= $subGrappleForce;
-    $subRating += $subGrappleForce;
     $subScore += $subGrappleForce;
     echo $turnNum.' : '.$subFullName.' '.$spacedictus[$proLingo]["grapple"].' ('.$subGrappleForce.') '.$objFullName.'<br>';
 }
